d7084175d64cdb90754c4f94b7007849cfd37c63
Paul Annekov <paul.annekov@gmail.com>
2020-04-25 13:38:49 +0300

    removed cloud photo data fetch, cooldown on multiple errors, single file save in any point of time

--- index.ts
+++ index.ts
@@ -23,6 +23,8 @@ process.on('unhandledRejection', (err) => {
 class AlbumSettings {
     private readonly ALBUM_SETTINGS_FILE = 'nostalgia.json';
     private readonly path: string;
+    private savePromise: Promise<void> | null;
+    private id: string;
     synced: { [key: string]: {
         id: string
     } };
@@ -36,19 +38,87 @@ class AlbumSettings {
         const contents = await fs.promises.readFile(this.path, { encoding: 'utf8', flag: 'a+' });
         const settings = contents ? JSON.parse(contents) : {};
         this.synced = settings.synced || {};
+        this.id = settings.id;
+    }
+
+    private async save() {
+        if (this.savePromise) {
+            await this.savePromise;
+            await this.save();
+            return;
+        }
+        this.savePromise = fs.promises.writeFile(this.path, JSON.stringify({
+            id: this.id,
+            synced: this.synced
+        }));
+        await this.savePromise;
+        this.savePromise = null;
+    }
+
+    async updateId(id: string) {
+        log.debug({ path: this.path }, 'updating album id in local settings');
+        this.id = id;
+        await this.save();
     }
 
     async updateSynced(name: string, id: string) {
-        log.debug({ path: this.path }, 'updating local album settings');
+        log.debug({ path: this.path }, 'updating synced list in local settings');
         this.synced[name] = { id };
-        await fs.promises.writeFile(this.path, JSON.stringify({
-            synced: this.synced
-        }));
+        await this.save();
+    }
+}
+
+class UploadQueue {
+    private readonly CONCURRENCY = 5;
+    private readonly MAX_CONSECUTIVE_ERRORS = 10;
+    private readonly FIRST_COOLDOWN = 10000;
+    private readonly queue: queue;
+    private consecutiveErrors = 0;
+    private cooldownTimer: NodeJS.Timeout | null;
+    private cooldown = this.FIRST_COOLDOWN;
+
+    constructor() {
+        this.queue = new queue({
+            concurrency: this.CONCURRENCY
+        });
+    }
+
+    get length() {
+        return this.queue.length;
+    }
+
+    execute() {
+        return promisify(this.queue.start).bind(this.queue)();
+    }
+
+    addJob(file: string, job: () => Promise<any>) {
+        this.queue.push(async () => {
+            try {
+                await job();
+                this.consecutiveErrors = 0;
+                this.cooldown = this.FIRST_COOLDOWN;
+            } catch(err) {
+                this.addJob(file, job);
+                this.consecutiveErrors++;
+                const isFinal = this.consecutiveErrors > this.MAX_CONSECUTIVE_ERRORS;
+                // too verbose shit, removing
+                delete err.config;
+                log.warn({ err, file, isFinal }, 'error during upload/append to album');
+                if (isFinal && !this.cooldownTimer) {
+                    log.info({ cooldownSeconds: this.cooldown / 1000 }, "too much errors, let's wait a bit");
+                    this.queue.stop();
+                    this.cooldownTimer = setTimeout(() => {
+                        this.cooldownTimer = null;
+                        this.queue.start();
+                    }, this.cooldown);
+                    this.cooldown *= 2;
+                }
+            }
+        })
     }
 }
 
 class OneWaySync {
-    private readonly CONCURRENCY = 4;
     private readonly APP_NAME = 'Nostalgia';
     // https://developers.google.com/photos/library/guides/upload-media#file-types-sizes
     private readonly SUPPORTED_TYPES = ['BMP', 'GIF', 'HEIC', 'ICO', 'JPG', 'PNG', 'TIFF', 'WEBP', 'RAW', '3GP', '3G2', 
@@ -107,37 +177,19 @@ class OneWaySync {
         let files: string[] = [];
         const entities = await fs.promises.readdir(path.resolve(source, subFolder), { withFileTypes: true });
         for (const entity of entities) {
+            const subPath = path.join(subFolder, entity.name);
             if (entity.isFile() && this.SUPPORTED_TYPES.includes(path.extname(entity.name).slice(1).toUpperCase())) {
-                files.push(path.join(subFolder, entity.name));
+                if ((await fs.promises.stat(path.resolve(source, subFolder, entity.name))).size == 0) {
+                    log.debug({ file: subPath }, 'zero-length file');
+                    continue;
+                }
+                files.push(subPath);
             } else if (entity.isDirectory()) {
-                files = files.concat(await this.getMediaFiles(source, path.join(subFolder, entity.name)));
+                files = files.concat(await this.getMediaFiles(source, subPath));
             }
         }
         return files;
     }
-
-    // private async fetchCloudAlbumPhotos(album: GPhotosAlbum, log: bunyan) {
-    //     const cloudAlbumInfo = await album.getInfo();
-    //     if (cloudAlbumInfo.itemsCount === 0) {
-    //         log.info('no photos in album');
-    //         return {};
-    //     }
-    //     log.info({ count: cloudAlbumInfo.itemsCount }, 'album already contain media');
-    //     let cloudMedia: { [key: string]: boolean; } = {};
-    //     while(true) {
-    //         const res = await album.fetchPhotoList({ cursor: null });
-    //         log.debug({ count: res.results.length }, 'found media items');
-    //         for (const photo of res.results) {
-    //             cloudMedia[photo.id] = true;
-    //         }
-    //         if (!res.nextCursor) {
-    //             break;
-    //         }
-    //     }
-    //     log.debug({ items: cloudMedia }, 'media items');
-
-    //     return cloudMedia;
-    // }
 
     private async uploadDirectory(directory: string) {
         log.info({ directory }, 'syncing directory');
@@ -151,18 +203,14 @@ class OneWaySync {
         }
         const albumSettings = new AlbumSettings(directoryPath);
         await albumSettings.loadSettings();
-
-        // log.info('fetching media from album');
-        // let cloudMedia = await this.fetchCloudAlbumPhotos(album, albumLog);
+        await albumSettings.updateId(album.id);
         
         const localMediaFiles = await this.getMediaFiles(directoryPath);
         log.info({ count: localMediaFiles.length }, 'found media files in directory');
         let ignored = 0;
         let added = 0;
         let uploadedBytes = 0;
-        const uploadQueue = new queue({
-            concurrency: this.CONCURRENCY
-        });
+        const uploadQueue = new UploadQueue();
         for (const file of localMediaFiles) {
             const filePath = path.resolve(directoryPath, file);
             const syncedFile = albumSettings.synced[file];
@@ -171,27 +219,24 @@ class OneWaySync {
                 ignored++;
                 continue;
             }
-            uploadQueue.push(async () => {
+            uploadQueue.addJob(file, async () => {
                 const size = (await fs.promises.stat(filePath)).size;
                 log.info({ file, size }, 'uploading new file');
-                try {
-            const photo = await this.gphotos.upload({
-                stream: fs.createReadStream(filePath),
-                        size,
-                filename: this.generateFilename(file),
+                const photo = await this.gphotos.upload({
+                    stream: fs.createReadStream(filePath),
+                    size,
+                    filename: this.generateFilename(file),
+                });
+                log.info({ file }, 'adding file to album');
+                await (album as GPhotosAlbum).append(photo);
+                uploadedBytes += size;
+                added++;
+                await albumSettings.updateSynced(file, photo.id);
             });
-            log.info({ file }, 'adding file to album');
-                    await (album as GPhotosAlbum).append(photo);
-                    uploadedBytes += size;
-            added++;
-                    await albumSettings.updateSynced(file, photo.id);
-                } catch(e) {
-                    log.error({ e, file }, 'error during upload/append to album');
-                }
-            })
         }
         log.info({ count: uploadQueue.length }, 'starting uploads');
-        await promisify(uploadQueue.start).bind(uploadQueue)();
+        await uploadQueue.execute();
+        // TODO: remove files in cloud that are removed locally
         log.info({ ignored, added, uploadedBytes }, 'directory synced');
     }
 
